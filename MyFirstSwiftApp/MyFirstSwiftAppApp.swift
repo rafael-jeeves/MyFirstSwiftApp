f99185f18c221d9a9031643a2f792d5efc6a4176
Rafael <rafael.evangelista@tryjeeves.com>
2025-08-07 10:04:06 -0300

    Initial Commit

---
+++ MyFirstSwiftApp/MyFirstSwiftAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirstSwiftAppApp.swift
+//  MyFirstSwiftApp
+//
+//  Created by Rafael Guerra Evangelista on 07/08/25.
+//
+
+import SwiftUI
+
+@main
+struct MyFirstSwiftAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
